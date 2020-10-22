a126d67d41e232eb2cd6adf544fc805819fc53ac
tytanus97 <tytanus97@gmail.com>
2020-10-22 21:17:43 +0200

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarComponent } from './components/car/car.component';
+import { AddOwnerComponent } from './components/owner/add-owner/add-owner.component';
+import { OwnerComponent } from './components/owner/owner.component';
+import { RocketComponent } from './components/rocket/rocket.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+{
+  path:'owners',component:OwnerComponent
+},
+{
+  path:'addOwner',component:AddOwnerComponent
+},
+{
+  path:'cars',component:CarComponent
+},
+{
+  path:'rockets',component:RocketComponent
+},{
+  path:'',redirectTo:'owners',pathMatch:'full'
+},
+{
+  path:"**",redirectTo:'owners'
+}];
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
