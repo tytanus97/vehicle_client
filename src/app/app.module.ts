0cc7db0a5d59da4b00cd48a2abde986114d6e9ea
tytanus97 <tytanus97@gmail.com>
2020-10-22 12:09:28 +0200

    config module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { OwnerService} from './services/owner.service';
+import { VehicleService } from './services/vehicle.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+export const services = [OwnerService,VehicleService];
 
 @NgModule({
   declarations: [
@@ -10,9 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [services,HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
