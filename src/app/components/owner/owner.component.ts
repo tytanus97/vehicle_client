08f8401edac634edd20c0415084dec4e72978c06
tytanus97 <tytanus97@gmail.com>
2020-10-22 21:15:16 +0200

    impement owner component

--- src/app/components/owner/owner.component.ts
+++ src/app/components/owner/owner.component.ts
@@ -1,6 +1,12 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { switchMap } from 'rxjs/operators'
 import { Owner } from 'src/app/models/Owner';
 import { OwnerService } from 'src/app/services/owner.service';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-owner',
@@ -11,7 +17,7 @@ export class OwnerComponent implements OnInit {
 
   public ownerList: Array<Owner>;
 
-  constructor(private ownerService: OwnerService) { }
+  constructor(private ownerService: OwnerService,private router: Router,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
 
@@ -26,4 +32,23 @@ export class OwnerComponent implements OnInit {
     });
   }
 
+  public deleteOwner(ownerId: number) {
+    
+    this.ownerService.deleteOwner(ownerId).subscribe(response => {
+      if(response.status === 200) {
+        console.log("Owner deletion confirmed");
+        window.location.reload();
+      } else throw Error('Something went wrong when deleting owner');
+    }),  
+    err => console.log(err)
+    }
+
+  public addOwner() {
+      this.router.navigate(['addOwner'],{queryParams: {action:'add'}})
+  }
+
+  public updateOwner(owner: Owner) {
+    this.ownerService.setCurrentOwner(owner);
+    this.router.navigate(['addOwner'],{queryParams: {action:'update'}});
+  }
 }
